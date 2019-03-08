d47d09f31c19e0feda8ac483c39c6d062c896e31
Charlotte Neff <charlotte.neff@gmail.com>
2019-03-08 17:24:47 +0000

    Automatically backed up by Learn

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -63,6 +63,26 @@ def exit_jukebox
 end
 
 def run(my_songs)
-  
+   help
+
+  input = ""
+  while input
+    puts "Please enter a command:"
+    input = gets.downcase.strip
+    case input
+    when 'list'
+      list(my_songs)
+    when 'play'
+      list(my_songs)
+      play(my_songs)
+    when 'help'
+      help
+    when 'exit'
+      exit_jukebox
+      break
+    else
+      help
+    end
+  end
   #this method is the same as in jukebox.rb
 end
